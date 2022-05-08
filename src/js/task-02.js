8ccaa0a3b470903d3305eca8c55aad8b56c55064
Yura-Voloshyn <yuriy_voloshyn@icloud.com>
2022-05-08 20:57:25 +0300

    task 1 and 2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,29 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const itemsList = document.querySelector('ul');
+itemsList.textContent = 'Ingredients:';
+
+// const items = ingredients.map(ingredient => {
+//   const item = document.createElement('li');
+//   item.textContent = ingredient;
+//   item.classList.add('item');
+//   return item;
+// });
+
+
+
+const createIngredient = args => {
+  return args.map(ingredient => {
+    const item = document.createElement('li');
+    item.textContent = ingredient;
+    item.classList.add('item');
+    return item;
+  });
+};
+const items = createIngredient(ingredients);
+
+
+console.log(items);
+itemsList.append(...items);
