cb5638e5fe841133b50acb25f3db5dd650c776c3
Yura-Voloshyn <yuriy_voloshyn@icloud.com>
2022-05-13 16:29:24 +0300

    t10 start

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,3 +1,22 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  input: document.querySelector('input'),
+  createBtn: document.querySelector('[data-create]'),
+  destroyBtn: document.querySelector('[data-destroy]'),
+  boxes: document.querySelector('#boxes'),
+};
+
+function getAmount() {
+  let amount = Number(refs.input.value);
+  console.log(amount);
+}
+
+refs.createBtn.addEventListener('click', onCreateBtnClick);
+refs.destroyBtn.addEventListener('click', onDestroyBtnClick);
+
+function onCreateBtnClick() {}
+
+function onDestroyBtnClick() {}
