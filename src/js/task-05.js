299f6d7be335ee17650c4a462a2cb1c2689a6262
Yura-Voloshyn <yuriy_voloshyn@icloud.com>
2022-05-13 08:05:35 +0300

    fin task 5

---
+++ src/js/task-05.js
@@ -0,0 +1,18 @@
+
+
+const refs = {
+    nameInput: document.querySelector('#name-input'),
+    nameOutput: document.querySelector('#name-output'),
+};
+console.log(refs.nameInput);
+console.log(refs.nameOutput);
+
+
+refs.nameInput.addEventListener("input", onNameInput);
+
+function onNameInput(event) {
+    if (event.currentTarget.value !== "") {
+        refs.nameOutput.textContent = event.currentTarget.value;
+    }
+};
+
