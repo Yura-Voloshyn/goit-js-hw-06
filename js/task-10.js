1c05365efcf07e85896cf643630ff03bc7ba7890
Yura-Voloshyn <yuriy_voloshyn@icloud.com>
2022-05-14 15:20:38 +0300

    push task 10

--- js/task-10.js
+++ js/task-10.js
@@ -7,16 +7,47 @@ const refs = {
   createBtn: document.querySelector('[data-create]'),
   destroyBtn: document.querySelector('[data-destroy]'),
   boxes: document.querySelector('#boxes'),
+  controls: document.querySelector('#controls'),
 };
 
-function getAmount() {
-  let amount = Number(refs.input.value);
-  console.log(amount);
-}
+// refs.controls.classList.add('item-tt');
 
-refs.createBtn.addEventListener('click', onCreateBtnClick);
+refs.createBtn.addEventListener('click', createPaletteItems);
 refs.destroyBtn.addEventListener('click', onDestroyBtnClick);
 
-function onCreateBtnClick() {}
+// function getAmount() {
+//   let amount = Number(refs.input.value);
+//   console.log(amount);
+// }
+function createPaletteItems() {
+  const items = [];
+  let amount = Number(refs.input.value);
+  for (let i = 0; i < amount; i += 1) {
+    const color = getRandomHexColor();
+    const item = document.createElement('div');
+    item.style.width = 30 + i * 10 + 'px';
+    item.style.height = 30 + i * 10 + 'px';
+    item.style.margin = 10 + 'px';
+    item.style.backgroundColor = color;
+    item.style.borderRadius = 4 + 'px';
+    // item.style.display = flex;
+    // item.style.flexDirection = row;
+    item.classList.add('item-tt');
+    items.push(item);
+  }
+  refs.boxes.append(...items);
+}
 
-function onDestroyBtnClick() {}
+// function onCreateBtnClick() {
+//   let box = document.createElement('div');
+//   box.style.width = 30 + 'px';
+//   box.style.height = 30 + 'px';
+//   box.style.margin = 10 + 'px';
+//   box.style.backgroundColor = getRandomHexColor();
+
+//   refs.boxes.appendChild(box, getAmount);
+// }
+
+function onDestroyBtnClick() {
+  refs.boxes.innerHTML = '';
+}
